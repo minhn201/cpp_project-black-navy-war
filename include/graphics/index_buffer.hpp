f286889df199faaa107918f4a35bf9559b428b0c
Carson Jennings <cjen02@gmail.com>
2024-08-06 14:07:48 -0700

    Adding headers and definitions for Vertex and index buffer classes

---
+++ include/graphics/index_buffer.hpp
@@ -0,0 +1,19 @@
+#ifndef INDEX_BUFFER_HPP
+#define INDEX_BUFFER_HPP
+
+class IndexBuffer {
+    private:
+        unsigned int m_RendererID;
+        unsigned int m_Count;
+
+    public:
+        IndexBuffer(const unsigned int* data, unsigned int count);
+        ~IndexBuffer();
+
+        void Bind() const;
+        void UnBind() const; 
+
+        inline unsigned int GetCount() const { return m_Count; }
+};
+
+#endif
---
+++ include/graphics/vertex_buffer.hpp
@@ -0,0 +1,16 @@
+#ifndef VERTEX_BUFFER_HPP
+#define VERTEX_BUFFER_HPP
+
+class VertexBuffer {
+    private:
+        unsigned int m_RendererID;
+
+    public:
+        VertexBuffer(const void* data, unsigned int size);
+        ~VertexBuffer();
+
+        void Bind() const;
+        void UnBind() const; 
+};
+
+#endif
