7ffaa5dcb5ac6753fa4a39d08e20da1dc0accb68
Minh Nguyen <minhn@uvic.ca>
2024-08-08 15:14:01 -0700

    Add folders for logics with Unit class

---
+++ include/logics/Unit.hpp
@@ -0,0 +1,31 @@
+#ifndef UNIT_HPP
+#define UNIT_HPP
+
+#include <string>
+#include <vector>
+
+class Unit {
+    public:
+
+        Unit(std::string type, bool is_player_unit);
+        ~Unit();
+
+        void attack(Unit& other);
+        void take_damage(float damage);
+        void move(float direction);
+        bool is_destroyed();
+
+        
+
+    private:
+        float pos_; //This is just an int in the range 0 to 100
+        float health_; // Most likely an int to represent the ships health
+        float damage_; // Most likely an into to represent how much damage the ship does
+        float speed_; // A floating point to represent the speed of the ship
+        std::string type_; // This could be a string or an enum type that contains the names of all ships
+        std::vector<float> shape_; // This could be a set of vertices that make up the ships bounding box
+        bool is_destroyed_;
+
+};
+
+#endif
---
+++ src/logics/Unit.cpp
@@ -0,0 +1,78 @@
+#include "logic/Unit.hpp"
+
+const std::string type_patrol = "Patrol Boat";
+const std::string type_destroyer = "Destroyer";
+const std::string type_submarine = "Submarine";
+const std::string type_bomber = "Bomber";
+const std::string type_helicopter = "Helicopter";
+
+const float health_patrol = 100.0f;
+const float health_destroyer = 100.0f;
+const float health_submarine = 100.0f;
+const float health_bomber = 100.0f;
+const float health_helicopter = 100.0f;
+
+const float speed_patrol = 0.10f;
+const float speed_destroyer = 0.10f;
+const float speed_submarine = 0.10f;
+const float speed_bomber = 0.10f;
+const float speed_helicopter = 0.10f;
+
+const float damage_patrol = 5.0f;
+const float damage_destroyer = 5.0f;
+const float damage_submarine = 5.0f;
+const float damage_bomber = 5.0f;
+const float damage_helicopter = 5.0f;
+
+ 
+Unit::Unit(std::string type, bool is_friendly): type_(type){
+   if(type_ == type_patrol){
+        health_ = health_patrol;
+        speed_ = speed_patrol;
+        damage_ = damage_patrol;      
+   }else if(type_ == type_destroyer){
+        health_ = health_destroyer;
+        speed_ = speed_destroyer;
+        damage_ = damage_destroyer;      
+   }else if(type_ == type_submarine){
+        health_ = health_submarine;
+        speed_ = speed_submarine;
+        damage_ = damage_submarine;      
+   }else if(type_ == type_bomber){
+        health_ = health_bomber;
+        speed_ = speed_bomber;
+        damage_ = damage_bomber;      
+   }else if(type_ == type_helicopter){
+        health_ = health_helicopter;
+        speed_ = speed_helicopter;
+        damage_ = damage_helicopter;      
+   }
+
+   if(is_friendly){
+        pos_ = 10.0f;
+   }else{
+        pos_ = 90.0f;
+   }
+};
+
+Unit::~Unit() = default;
+
+void Unit::take_damage(float damage){
+    health_ -= damage;
+    if (health_ <= 0){
+        is_destroyed_ = true;
+    }
+};
+
+void Unit::attack(Unit& other){
+    other.take_damage(damage_);
+
+};
+        
+void Unit::move(float direction){
+    pos_+=direction;
+};
+        
+bool Unit::is_destroyed(){
+    return is_destroyed_;
+};
