b39f898800fd932c753a24dc218cc30996523b45
Minh Nguyen <minhn@uvic.ca>
2024-08-11 00:13:29 -0700

    Improve attack mechanism with different weaponary

--- include/logics/Unit.hpp
+++ include/logics/Unit.hpp
@@ -6,33 +6,31 @@
 #include "logics/naval_base.hpp"
 #include "graphics/renderer.hpp"
 
-class Unit {
-    public:
+class Unit
+{
+public:
+    Unit(std::string type, bool is_player_unit);
+    ~Unit();
 
-        Unit(std::string type, bool is_player_unit);
-        ~Unit();
+    void attack(Unit &other);
+    void attack(NavalBase &other);
+    void take_damage(float damage);
+    void move(float direction);
+    bool is_destroyed();
+    float get_position();
+    std::string get_type();
+    float get_speed();
+    std::vector<float> get_window_position(RenderWindow &render_window);
+    bool shouldOccur(float probability);
 
-        void attack(Unit& other);
-        void attack(NavalBase& other);
-        void take_damage(float damage);
-        void move(float direction);
-        bool is_destroyed();
-        float get_position();
-        std::string get_type();
-        float get_speed();
-        std::vector<float> get_window_position(RenderWindow &render_window);
-
-        
-
-    private:
-        float pos_; //This is just an int in the range 0 to 100
-        float health_; // Most likely an int to represent the ships health
-        float damage_; // Most likely an into to represent how much damage the ship does
-        float speed_; // A floating point to represent the speed of the ship
-        std::string type_; // This could be a string or an enum type that contains the names of all ships
-        std::vector<float> shape_; // This could be a set of vertices that make up the ships bounding box
-        bool is_destroyed_;
-
+private:
+    float pos_;                // This is just an int in the range 0 to 100
+    float health_;             // Most likely an int to represent the ships health
+    float damage_;             // Most likely an into to represent how much damage the ship does
+    float speed_;              // A floating point to represent the speed of the ship
+    std::string type_;         // This could be a string or an enum type that contains the names of all ships
+    std::vector<float> shape_; // This could be a set of vertices that make up the ships bounding box
+    bool is_destroyed_;
 };
 
 #endif
--- src/logics/Unit.cpp
+++ src/logics/Unit.cpp
@@ -1,5 +1,8 @@
 #include "logics/Unit.hpp"
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <random>
 
 const std::string type_patrol = "Patrol Boat";
 const std::string type_destroyer = "Destroyer";
@@ -8,132 +11,260 @@ const std::string type_bomber = "Bomber";
 const std::string type_helicopter = "Helicopter";
 
 const float health_patrol = 100.0f;
-const float health_destroyer = 100.0f;
-const float health_submarine = 100.0f;
-const float health_bomber = 100.0f;
-const float health_helicopter = 100.0f;
+const float health_destroyer = 300.0f;
+const float health_submarine = 150.0f;
+const float health_bomber = 60.0f;
+const float health_helicopter = 30.0f;
 
 const float speed_patrol = 0.01f;
-const float speed_destroyer = 0.001f;
-const float speed_submarine = 0.005f;
-const float speed_bomber = 0.01f;
+const float speed_destroyer = 0.004f;
+const float speed_submarine = 0.007f;
+const float speed_bomber = 0.02f;
 const float speed_helicopter = 0.005f;
 
-const float damage_patrol = 0.5f;
-const float damage_destroyer = 0.5f;
-const float damage_submarine = 0.5f;
-const float damage_bomber = 0.5f;
-const float damage_helicopter = 0.5f;
+const float damage_mg = 0.5f;
+const float damage_topedo = 2.0f;
+const float damage_depth_charge = 3.0f;
 
+std::vector<float> patrol_shape{
+    0.0f, 0.0f, // bottom left
+    2.0f, 0.0f, // bottom right
+    2.0f, 0.1f, // top right
+    0.0f, 0.1f  // top left
+};
+std::vector<float> destroyer_shape{
+    0.0f, 0.0f, // bottom left
+    3.0f, 0.0f, // bottom right
+    3.0f, 0.2f, // top right
+    0.0f, 0.2f  // top left
+};
+std::vector<float> submarine_shape{
+    0.0f, -0.4f, // bottom left
+    2.0f, -0.4f, // bottom right
+    2.0f, -0.3f, // top right
+    0.0f, -0.3f  // top left
+};
+std::vector<float> bomber_shape{
+    0.0f, 0.5f, // bottom left
+    2.0f, 0.5f, // bottom right
+    2.0f, 0.6f, // top right
+    0.0f, 0.6f  // top left
+};
+std::vector<float> helicopter_shape{
+    0.0f, 0.4f, // bottom left
+    1.0f, 0.4f, // bottom right
+    1.0f, 0.5f, // top right
+    0.0f, 0.5f  // top left
+};
 
-std::vector<float> patrol_shape {
-     0.0f, 0.0f, // bottom left
-     2.0f, 0.0f, // bottom right
-     2.0f, 0.1f, // top right
-     0.0f, 0.1f // top left
-};
-std::vector<float> destroyer_shape {
-     0.0f, 0.0f,  // bottom left
-     3.0f, 0.0f,  // bottom right
-     3.0f, 0.2f,  // top right
-     0.0f, 0.2f  // top left
-};
-std::vector<float> submarine_shape {
-     0.0f, -0.4f,  // bottom left
-     2.0f, -0.4f,  // bottom right
-     2.0f, -0.3f,  // top right
-     0.0f, -0.3f  // top left
-};
-std::vector<float> bomber_shape {
-     0.0f, 0.5f,  // bottom left
-     2.0f, 0.5f,  // bottom right
-     2.0f, 0.6f,  // top right
-     0.0f, 0.6f  // top left
-};
-std::vector<float> helicopter_shape {
-     0.0f, 0.4f,  // bottom left
-     1.0f, 0.4f,  // bottom right
-     1.0f, 0.5f,  // top right
-     0.0f, 0.5f  // top left
-};
- 
-Unit::Unit(std::string type, bool is_friendly): type_(type){
-   if(type_ == type_patrol){
-        health_ = health_patrol;
-        speed_ = speed_patrol;
-        damage_ = damage_patrol;  
-        shape_ = patrol_shape;    
-   }else if(type_ == type_destroyer){
-        health_ = health_destroyer;
-        speed_ = speed_destroyer;
-        damage_ = damage_destroyer;  
-        shape_ = destroyer_shape;   
-   }else if(type_ == type_submarine){
-        health_ = health_submarine;
-        speed_ = speed_submarine;
-        damage_ = damage_submarine;
-        shape_ = submarine_shape;      
-   }else if(type_ == type_bomber){
-        health_ = health_bomber;
-        speed_ = speed_bomber;
-        damage_ = damage_bomber; 
-        shape_ = bomber_shape;     
-   }else if(type_ == type_helicopter){
-        health_ = health_helicopter;
-        speed_ = speed_helicopter;
-        damage_ = damage_helicopter;
-        shape_ = helicopter_shape;      
-   }
+Unit::Unit(std::string type, bool is_friendly) : type_(type)
+{
+     if (type_ == type_patrol)
+     {
+          health_ = health_patrol;
+          speed_ = speed_patrol;
+          shape_ = patrol_shape;
+     }
+     else if (type_ == type_destroyer)
+     {
+          health_ = health_destroyer;
+          speed_ = speed_destroyer;
+          shape_ = destroyer_shape;
+     }
+     else if (type_ == type_submarine)
+     {
+          health_ = health_submarine;
+          speed_ = speed_submarine;
+          shape_ = submarine_shape;
+     }
+     else if (type_ == type_bomber)
+     {
+          health_ = health_bomber;
+          speed_ = speed_bomber;
+          shape_ = bomber_shape;
+     }
+     else if (type_ == type_helicopter)
+     {
+          health_ = health_helicopter;
+          speed_ = speed_helicopter;
+          shape_ = helicopter_shape;
+     }
 
-   if(is_friendly){
-        pos_ = 10.0f;
-   }else{
-        pos_ = 90.0f;
-   }
+     // Hopefully defenseless enough!
+     damage_ = 0.001f;
+
+     if (is_friendly)
+     {
+          pos_ = 0.0f;
+     }
+     else
+     {
+          pos_ = 100.0f;
+     }
 };
 
 Unit::~Unit() = default;
 
-void Unit::take_damage(float damage){
-    health_ -= damage;
-    if (health_ <= 0){
-        is_destroyed_ = true;
-    }
+void Unit::take_damage(float damage)
+{
+     health_ -= damage;
+     if (health_ <= 0)
+     {
+          is_destroyed_ = true;
+     }
 };
 
-void Unit::attack(Unit& other){
-    other.take_damage(damage_);
+void Unit::attack(Unit &other)
+{
+     if (type_ == type_patrol)
+     {
+          // add 0.1s delay
+          // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+          if (other.type_ == type_bomber || other.type_ == type_helicopter)
+          {
+               if (!shouldOccur(0.8f))
+               {
+                    return;
+               }
+               damage_ = damage_mg;
+          }
+          else if (other.type_ != type_submarine)
+          {
+               damage_ = damage_mg;
+          }
+     }
+     else if (type_ == type_destroyer)
+     {
+          if (other.type_ != type_submarine)
+          {
+               // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+               if (other.type_ == type_bomber || other.type_ == type_helicopter)
+               {
+                    if (!shouldOccur(0.8f))
+                    {
+                         return;
+                    }
+               }
 
+               damage_ = damage_mg;
+          }
+          else
+          {
+               // damages may be inflicted 20%, 50%, 100% on submarines
+               // std::this_thread::sleep_for(std::chrono::seconds(3));
+               float random_value = std::rand() / static_cast<float>(RAND_MAX);
+               if (random_value < 0.2f)
+               {
+                    damage_ = 0.2f * damage_depth_charge; // 20% damage
+               }
+               else if (random_value < 0.7f)
+               {
+                    damage_ = 0.5f * damage_depth_charge; // 50% damage
+               }
+               else
+               {
+                    damage_ = damage_depth_charge; // 100% damage
+               }
+          }
+     }
+     else if (type_ == type_submarine)
+     {
+          if (other.type_ != type_bomber || other.type_ != type_helicopter)
+          {
+               // std::this_thread::sleep_for(std::chrono::seconds(3));
+               damage_ = damage_topedo;
+          }
+     }
+     else if (type_ == type_helicopter)
+     {
+          if (other.type_ == type_submarine)
+          {
+               float random_value = std::rand() / static_cast<float>(RAND_MAX);
+               if (random_value < 0.2f)
+               {
+                    damage_ = 0.2f * damage_depth_charge; // 20% damage
+               }
+               else if (random_value < 0.7f)
+               {
+                    damage_ = 0.5f * damage_depth_charge; // 50% damage
+               }
+               else
+               {
+                    damage_ = damage_depth_charge; // 100% damage
+               }
+          }
+          else
+          {
+               if (other.type_ == type_helicopter || other.type_ == type_bomber)
+               {
+                    if (!shouldOccur(0.8f))
+                    {
+                         return;
+                    }
+               }
+               damage_ = damage_mg;
+          }
+     }
+     else if (type_ == type_bomber)
+     {
+          if (other.type_ != type_bomber || other.type_ != type_helicopter)
+          {
+               damage_ = damage_topedo;
+          }
+     }
+
+     other.take_damage(damage_);
 };
 
-void Unit::attack(NavalBase& other) {
+void Unit::attack(NavalBase &other)
+{
      other.take_damage(damage_);
 }
-        
-void Unit::move(float direction){
-    pos_+=direction;
-};
-        
-bool Unit::is_destroyed(){
-    return is_destroyed_;
+
+void Unit::move(float direction)
+{
+     pos_ += direction;
 };
 
-float Unit::get_position() {
+bool Unit::is_destroyed()
+{
+     return is_destroyed_;
+};
+
+float Unit::get_position()
+{
      return pos_;
 }
 
-std::string Unit::get_type() {
+std::string Unit::get_type()
+{
      return type_;
 }
 
-float Unit::get_speed() {
+float Unit::get_speed()
+{
      return speed_;
 }
 
-std::vector<float> Unit::get_window_position(RenderWindow &render_window) {
+bool Unit::shouldOccur(float probability)
+{
+     // Obtain a random number from hardware
+     std::random_device rd;
+     // Seed the generator
+     std::mt19937 gen(rd());
+     // Generate a float number between 0.0 and 1.0
+     std::uniform_real_distribution<> distr(0.0, 1.0);
+
+     float random_value = distr(gen);
+     return random_value < probability;
+}
+
+std::vector<float> Unit::get_window_position(RenderWindow &render_window)
+{
      std::vector<float> window_position = shape_; // copy over the shape
-     
-     for (int i = 0; i < shape_.size(); i += 2) { // Loop over the x values only
+
+     for (int i = 0; i < shape_.size(); i += 2)
+     {                                   // Loop over the x values only
           window_position.at(i) += pos_; // to get the window position we simply add the offset to the shapes x values
      }
 
